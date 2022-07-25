25fdd147ce726737152d4ccf168391e747c4fccd
rodrigoalbues <95535393+rodrigoalbues@users.noreply.github.com>
2022-07-25 12:41:06 -0400

    Initial commit

---
+++ ContatosBootcamp/app/src/main/java/me/dio/contatosbootcamp/Contact.kt
@@ -0,0 +1,3 @@
+package me.dio.contatosbootcamp
+
+data class Contact (val name: String, val phoneNumber: String)
---
+++ README.md
@@ -0,0 +1,2 @@
+# Recursos-Nativos-do-Android
+ Recursos Nativos do Android (Agenda, Contatos, Fotos e Camera)
